28d797510b2f204b71f4c87cfabe9195c7a51fc4
how2jollyg <guilhem.jolly@gmail.com>
2025-04-21 19:47:59 +0200

    adding new int model and mart

--- models/intermediate/_int_customer_favorite_product.sql
+++ models/intermediate/_int_customer_favorite_product.sql
@@ -1,18 +1,23 @@
 with customer_product_quantities as (
     select
         customer_id,
-        product_id,
+        oi.product_id,
+        product_name,
         sum(item_quantity) as total_quantity
-    from {{ ref('_int_order_items') }}
+    from {{ ref('_int_order_items') }} oi
+    left join {{ ref('stg_products')}} p
+    on oi.product_id = p.product_id
     group by 
         customer_id,
-        product_id
+        oi.product_id,
+        product_name
 ),
 
 ranked_products as (
     select
         customer_id,
         product_id,
+        product_name,
         total_quantity,
         ROW_NUMBER() OVER(
             PARTITION BY customer_id
@@ -22,6 +27,8 @@ ranked_products as (
 )
 select
     customer_id,
-    product_id as favorite_product_id
+    product_id as favorite_product_id,
+    product_name as favorite_product
+
 from ranked_products
 where rank = 1
